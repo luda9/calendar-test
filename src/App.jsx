09ef118e7dd956dea59c7129324d8478b33fc3fa
Daniel Santillan <ldms99@hotmail.com>
2025-03-13 14:09:45 -0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import dayjs from 'dayjs';
 import 'dayjs/locale/es';
 import { Button, Modal, ModalDialog, ModalClose, Typography, Container } from '@mui/joy';
-
+import Navbar from './components/Navbar';
 
 function App() {
   const [ selectedDate, setSelectedDate ] = useState('')
@@ -32,59 +32,64 @@ function App() {
 
   return (
     <div
-      style={{width: '100vw', display: 'flex', flexDirection: 'column', alignContent: 'center' }}
+      style={{width: '100vw', display: 'flex', flexDirection: 'column', alignContent: 'center', height: '100vh' }}
     >
-      <Typography
-              component="h1"
-              id="modal-title"
-              level="h1"
-              textColor="inherit"
-              sx={{ fontWeight: 'lg', mb: 3, textAlign: 'center' }}
-            >Eventos</Typography>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar
-          disablePast
-          shouldDisableDate={disableSpecificDates}
-          sx={{
-            '& .Mui-disabled': {
-              backgroundColor: '#f0f0f0',
-              pointerEvents: 'none',
-            },
-          }}
-          onChange={(date) => setSelectedDate(date.locale('es').format('dddd DD [de] MMMM [del] YYYY'))}
-        />
-        {selectedDate && (
-          <Button
-            style={{width: 100, margin: '0 auto'}}
-            color="neutral"
-            onClick={() => setOpenModal(true)}
-            variant="soft"
-          >Continue</Button>
-        )}
-        <Modal
-          open={openModal}
-          onClose={() => setOpenModal(false)}
-        >
-          <ModalDialog>
-            <ModalClose />
-            <Typography
-              component="h2"
-              id="modal-title"
-              level="h4"
-              textColor="inherit"
-              sx={{ fontWeight: 'lg', mb: 1 }}
-            >Confirmación</Typography>
-            <Typography id="modal-desc" textColor="text.tertiary">
-              Quiero apartar la siguiente fecha: <code>{selectedDate}</code>.
-            </Typography>
+      <Navbar />
+      <div
+        style={{width: '100%', display: 'flex', flexDirection: 'column', height: '100%' }}
+      >
+        <Typography
+                component="h1"
+                id="modal-title"
+                level="h1"
+                textColor="inherit"
+                sx={{ fontWeight: 'lg', mb: 3, textAlign: 'center' }}
+              >Eventos</Typography>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateCalendar
+            disablePast
+            shouldDisableDate={disableSpecificDates}
+            sx={{
+              '& .Mui-disabled': {
+                backgroundColor: '#f0f0f0',
+                pointerEvents: 'none',
+              },
+            }}
+            onChange={(date) => setSelectedDate(date.locale('es').format('dddd DD [de] MMMM [del] YYYY'))}
+          />
+          {selectedDate && (
             <Button
-              color="danger"
-              onClick={handleClick}
+              style={{width: 100, margin: '0 auto'}}
+              color="neutral"
+              onClick={() => setOpenModal(true)}
               variant="soft"
-          >Continue</Button>
-          </ModalDialog>
-        </Modal>
-      </LocalizationProvider>
+            >Continue</Button>
+          )}
+          <Modal
+            open={openModal}
+            onClose={() => setOpenModal(false)}
+          >
+            <ModalDialog>
+              <ModalClose />
+              <Typography
+                component="h2"
+                id="modal-title"
+                level="h4"
+                textColor="inherit"
+                sx={{ fontWeight: 'lg', mb: 1 }}
+              >Confirmación</Typography>
+              <Typography id="modal-desc" textColor="text.tertiary">
+                Quiero apartar la siguiente fecha: <code>{selectedDate}</code>.
+              </Typography>
+              <Button
+                color="danger"
+                onClick={handleClick}
+                variant="soft"
+            >Continue</Button>
+            </ModalDialog>
+          </Modal>
+        </LocalizationProvider>
+      </div>
     </div>
   );
 }
